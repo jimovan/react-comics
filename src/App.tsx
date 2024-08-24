d8c4687782cf5325f1625b290b80be940256fec3
Jim Donovan <jimdon326@hotmail.co.uk>
2024-08-24 20:10:50 +0100

    Add env.local for testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const testKey = import.meta.env.VITE_TEST_KEY;
+
   return (
     <>
       <div>
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>{testKey}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
